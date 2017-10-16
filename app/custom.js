1a65e0734af28d49a56b586f2f0d518f375c32b2
EC2 Default User <ec2-user@ip-172-31-30-54.us-west-2.compute.internal>
2017-10-16 21:29:55 +0000

    deployment wip

---
+++ app/custom.js
@@ -0,0 +1,7 @@
+events.on('app_initialized.NotebookApp', function () {
+    console.log('custom.js: hiding header and toolbar');
+    $('#header-container').toggle();
+    $('.header-bar').toggle();
+    $('div#maintoolbar').toggle();
+    events.trigger('resize-header.Page');
+});
